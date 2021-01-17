62a97bbfcb1be986d50495c4ef3561309bd08272
john <jswindlehurst@gmail.com>
2021-01-17 15:50:43 -0700

    Add some Jimja features

--- main.py
+++ main.py
@@ -7,17 +7,13 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route('/aboutme')
-def aboutme():
-    return render_template("aboutme.html")
-
-@app.route('/portfolio')
-def portfolio():
-    return render_template("portfolio.html")
+@app.route("/about-me")
+def about():
+    return render_template("about.html")
 
 
 
 
 
-if__name__ == '__main__':
+if __name__ == '__main__':
     app.run()
