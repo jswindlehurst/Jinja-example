df8d5bc2daadf38e1b6832c0110e2f61f022339f
john <jswindlehurst@gmail.com>
2021-01-21 13:24:46 -0700

    Adding some Jinja features

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, make_response
 import datetime
 
 
@@ -14,10 +14,27 @@ def index():
 
     return render_template("index.html", some_text=some_text, current_year=current_year, cities=cities)
 
-@app.route("/about-me")
+@app.route("/about-me", methods=["GET"])
 def about():
-    return render_template("about.html")
+    if request.method == "GET":
+        user_name = request.cookies.get("user_name")
+        return render_template("about.html", name=user_name)
 
+@app.route("/contact", methods=["POST"])
+def contact():
+
+    contact_name = request.form.get("contact-name")
+    contact_email = request.form.get("contact-email")
+    contact_message = request.form.get("contact-message")
+
+    print(contact_name)
+    print(contact_email)
+    print(contact_message)
+
+    response = make_response(render_template("success.html"))
+    response.set_cookie("user_name", contact_name)
+
+    return response
 
 
 
